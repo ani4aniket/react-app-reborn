c012530f08cb874c1f47568b5c8659b791ee416d
Aniket Kumar <aniketkumar049@gmail.com>
2020-01-25 23:16:34 +0530

    Replacing Link with NavLink

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Posts from './Posts/Posts';
 import './Blog.css';
-import { Route, Link  } from 'react-router-dom';
+import { Route, NavLink  } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
@@ -15,12 +15,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/" exact>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
